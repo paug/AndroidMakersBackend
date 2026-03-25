8244d8718fc133a7b91ff7e5bff8be99881e984d
Martin Bonnin <martin@mbonnin.net>
2026-03-25 14:20:41 +0100

    Remove GraphQL prefix in the schema

--- service/src/main/kotlin/androidmakers/service/graphql/scalars.kt
+++ service/src/main/kotlin/androidmakers/service/graphql/scalars.kt
@@ -4,6 +4,7 @@ import com.apollographql.apollo.ast.GQLStringValue
 import com.apollographql.apollo.ast.GQLValue
 import com.apollographql.apollo.execution.Coercing
 import com.apollographql.apollo.execution.ExternalValue
+import com.apollographql.execution.annotation.GraphQLName
 import com.apollographql.execution.annotation.GraphQLScalar
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
@@ -13,6 +14,7 @@ import kotlinx.datetime.LocalDateTime
  * A type representing a formatted kotlinx.datetime.Instant
  */
 @GraphQLScalar(GraphQLInstantCoercing::class)
+@GraphQLName("Instant")
 typealias GraphQLInstant = Instant
 
 object GraphQLInstantCoercing : Coercing<Instant> {
@@ -20,8 +22,8 @@ object GraphQLInstantCoercing : Coercing<Instant> {
         return Instant.parse(value as String)
     }
 
-    override fun parseLiteral(gqlValue: GQLValue): Instant {
-        return Instant.parse((gqlValue as GQLStringValue).value)
+    override fun parseLiteral(value: GQLValue): Instant {
+        return Instant.parse((value as GQLStringValue).value)
     }
 
     override fun serialize(internalValue: Instant): ExternalValue {
@@ -30,6 +32,7 @@ object GraphQLInstantCoercing : Coercing<Instant> {
 }
 
 @GraphQLScalar(GraphQLLocalDateCoercing::class)
+@GraphQLName("LocalDate")
 typealias GraphQLLocalDate = LocalDate
 
 object GraphQLLocalDateCoercing : Coercing<LocalDate> {
@@ -37,8 +40,8 @@ object GraphQLLocalDateCoercing : Coercing<LocalDate> {
         return LocalDate.parse(value as String)
     }
 
-    override fun parseLiteral(gqlValue: GQLValue): LocalDate {
-        return LocalDate.parse((gqlValue as GQLStringValue).value)
+    override fun parseLiteral(value: GQLValue): LocalDate {
+        return LocalDate.parse((value as GQLStringValue).value)
     }
 
     override fun serialize(internalValue: LocalDate): ExternalValue {
@@ -47,6 +50,7 @@ object GraphQLLocalDateCoercing : Coercing<LocalDate> {
 }
 
 @GraphQLScalar(GraphQLLocalDateTimeCoercing::class)
+@GraphQLName("LocalDateTime")
 typealias GraphQLLocalDateTime = LocalDateTime
 
 object GraphQLLocalDateTimeCoercing : Coercing<LocalDateTime> {
@@ -54,8 +58,8 @@ object GraphQLLocalDateTimeCoercing : Coercing<LocalDateTime> {
         return LocalDateTime.parse(value as String)
     }
 
-    override fun parseLiteral(gqlValue: GQLValue): LocalDateTime {
-        return LocalDateTime.parse((gqlValue as GQLStringValue).value)
+    override fun parseLiteral(value: GQLValue): LocalDateTime {
+        return LocalDateTime.parse((value as GQLStringValue).value)
     }
 
     override fun serialize(internalValue: LocalDateTime): ExternalValue {
